7376afdb047126a6351b5284242be4c379c9e0fd
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-09-17 00:06:14 -0300

    CheckMate logic added

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -43,10 +43,14 @@ public class Program {
                 sc.nextLine();
             }
             catch (InputMismatchException e) {
-                System.out.println(e.getMessage());
+                System.out.println(e.getMessage() + " ");
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
+        sc.nextLine();
+        sc.close();
 
     }
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -61,8 +61,16 @@ public class UI {
         System.out.println();
         printCapturedPieces(captured);
         System.out.println();
-        System.out.println("Turn: " + chessMatch.getTurn() + "\nWaiting player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()) System.out.println("CHECK!");
+        System.out.println("Turn: " + chessMatch.getTurn());
+
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) System.out.println("CHECK!");
+        }
+        else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
